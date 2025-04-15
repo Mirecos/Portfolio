6c33e48971c346cbb166e46cf1beee518d30ddbc
Michel <michelrecchia1@gmail.com>
2025-04-15 09:24:39 +0200

    Change font + add SmartPlanner project

--- src/data/Projects.ts
+++ src/data/Projects.ts
@@ -1,6 +1,7 @@
 import WebPortfolio from "../assets/Projects/WebPortfolio.png";
 import MRDesign from "../assets/Projects/mr-design.png";
 import ETHDashboard from "../assets/Projects/ETH-Dashboard.png";
+import SmartPlanner from "../assets/Projects/SmartPlanner.webp";
 
 
 export const Projects = [
@@ -21,5 +22,11 @@ export const Projects = [
         "description": "A react-native library I started to create to improve overall development experience. It is a collection of components and hooks that i use in my projects. It is still in development, but it is already usable.",
         "year": "2025",
         "image": MRDesign
+    },
+    {
+        "name": "Restaurant management assistance app",
+        "description": "In collaboration with two friends, and a restaurant in Metz, we started working on a web application which aims to help them with many tasks like planning management, team management, and more. It is still in progress.",
+        "year": "2024",
+        "image": SmartPlanner
     }
 ]
